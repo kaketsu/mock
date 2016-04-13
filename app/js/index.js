c27664a696b52cbe5124f7710e6cba05df598ef3
jiehua <jiehua@ebay.com>
2016-04-13 18:18:24 +0800

    add template

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,26 @@
-var mainApp = angular.module('mainApp',[]);
+var mainApp = angular.module('mainApp',['ngRoute']);
+mainApp.config(function($routeProvider){
+	$routeProvider
+	.when('/',{
+		templateUrl : 'template/home.html',
+     	controller  : 'mainCtrl'
+	})
+	.when('/about',{
+		templateUrl : 'template/about.html',
+      	controller  : 'aboutCtrl'
+	})
+	.when('/contact',{
+		templateUrl : 'template/contact.html',
+      	controller  : 'contactCtrl'
+	})
+})
+
 mainApp.controller('mainCtrl',function($scope){
 	$scope.message = 'Hello, this is a main controller';
+})
+mainApp.controller('aboutCtrl',function($scope){
+	$scope.message = 'Hello, this is a about controller';
+})
+mainApp.controller('contactCtrl',function($scope){
+	$scope.message = 'Hello, this is a contact controller';
 })
