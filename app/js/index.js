0b4d4db60b02191edfad267b0006101e13aa95f8
jiehua <jiehua@ebay.com>
2016-04-14 14:39:44 +0800

    restangular

--- app/js/index.js
+++ app/js/index.js
@@ -12,14 +12,36 @@ mainApp.config(function($routeProvider,RestangularProvider){
 	.when('/contact',{
 		templateUrl : 'template/contact.html',
       	controller  : 'contactCtrl'
-	})
+	});
+	RestangularProvider.setBaseUrl('http://mock.ebay.com/');
 })
 
 mainApp.controller('mainCtrl',function($scope){
 	$scope.message = 'Hello, this is a main controller';
 })
-mainApp.controller('aboutCtrl',function($scope){
+mainApp.controller('aboutCtrl',function($scope,Restangular){
 	$scope.message = 'Hello, this is a about controller';
+
+	
+	//基本用法
+	/*var info = Restangular.one('data','info.json');
+	info.get().then(function(data){
+		console.log(data);
+	})
+
+	var mates = Restangular.one('data','mates.json');
+	mates.getList().then(function(data){
+		console.log(data);
+	})*/
+
+	var rest = Restangular.all('data');
+	rest.one('info.json').get().then(function(data){
+		console.log(data);
+	})
+
+	rest.one('mates.json').getList().then(function(data){
+		console.log(data);
+	})
 })
 mainApp.controller('contactCtrl',function($scope){
 	$scope.message = 'Hello, this is a contact controller';
